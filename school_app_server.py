963f7c340b2ad35bdb08eac7ff8e7f22582ba1cc
Christian Wichmann <wichmann@bbs-os-brinkstr.de>
2024-05-06 20:47:46 +0200

    Fix linter problems and add doc-strings.

--- school_app_server.py
+++ school_app_server.py
@@ -1,11 +1,28 @@
 #! /usr/bin/env python3
 
+"""
+Configures and prepares Docker Compose scripts to setup a schools app server.
+It provides Docker stacks containing the following services:
+
+* Traefik
+* Portainer
+* Watchtower
+* Nextcloud
+* Moodle
+* Kanboard
+* Etherpad
+* Hedgedoc
+* StirlingPDF
+* Homer
+* Heimdall
+"""
+
 import re
 import sys
 import string
 import secrets
-import logging
 import fileinput
+import logging
 import logging.handlers
 from pathlib import Path
 from argparse import ArgumentParser
@@ -14,15 +31,13 @@ from argparse import ArgumentParser
 python_print = print
 
 import yaml
-from yaspin import yaspin
 from python_on_whales import DockerClient
 from prompt_toolkit.key_binding import KeyBindings
-from prompt_toolkit.shortcuts import input_dialog, checkboxlist_dialog, yes_no_dialog
+from prompt_toolkit.shortcuts import checkboxlist_dialog, yes_no_dialog
 from prompt_toolkit.styles import Style
 from prompt_toolkit.completion import NestedCompleter
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.validation import Validator
-from prompt_toolkit.history import InMemoryHistory
 from prompt_toolkit import print_formatted_text as print
 from prompt_toolkit import PromptSession, prompt, HTML
 
@@ -34,7 +49,7 @@ APP = 'SchoolAppServer'
 VERSION = '1.0'
 INITIAL_SETUP_MARKER_FILE = '.initial_setup_complete'
 
-infrastructure_env = """UPTIMEKUMA_IMAGE=louislam/uptime-kuma:1
+INFRASTRUCTURE_ENV = """UPTIMEKUMA_IMAGE=louislam/uptime-kuma:1
 UPTIMEKUMA_DOMAIN=status.{domain}
 TRAEFIK_IMAGE=traefik:v2.11
 TRAEFIK_DASHBOARD_DOMAIN=dashboard.{domain}
@@ -44,24 +59,24 @@ PORTAINER_DOMAIN=portainer.{domain}
 WATCHTOWER_IMAGE=containrrr/watchtower:latest
 """
 
-nextcloud_env = """NEXTCLOUD_IMAGE=nextcloud:27.1-apache
+NEXTCLOUD_ENV = """NEXTCLOUD_IMAGE=nextcloud:27.1-apache
 NEXTCLOUD_DOMAIN=nextcloud.{domain}
 NEXTCLOUD_DB_IMAGE=mariadb:10.6
 NEXTCLOUD_REDIS_IMAGE=redis:latest
 """
 
-kanboard_env = """KANBOARD_IMAGE=kanboard/kanboard:v1.2.36
+KANBOARD_ENV = """KANBOARD_IMAGE=kanboard/kanboard:v1.2.36
 KANBOARD_DOMAIN=kanboard.{domain}
 KANBOARD_SMTP_ADDR={kanboard_smtp_addr}
 KANBOARD_SMTP_PORT=465
 KANBOARD_SMTP_USER={kanboard_smtp_user}
 """
 
-tools_env = """STIRLINGPDF_IMAGE=frooodle/s-pdf:latest
+TOOLS_ENV = """STIRLINGPDF_IMAGE=frooodle/s-pdf:latest
 STIRLINGPDF_DOMAIN=pdf.{domain}
 """
 
-moodle_env = """MOODLE_IMAGE=bitnami/moodle:latest
+MOODLE_ENV = """MOODLE_IMAGE=bitnami/moodle:latest
 MOODLE_DOMAIN1=moodle.{domain}
 MOODLE_DOMAIN2=moodle2.{domain}
 MOODLE_DB_IMAGE=mariadb:latest
@@ -72,7 +87,7 @@ MOODLE_ADMIN_EMAIL={moodle_admin_mail_address}
 MOODLE_SITE_NAME={moodle_site_name}
 """
 
-static_env = """
+STATIC_ENV = """
 HEIMDALL_IMAGE=linuxserver/heimdall:latest
 HEIMDALL_DOMAIN=www.{domain}
 HOMER_IMAGE=b4bz/homer:latest
@@ -81,12 +96,12 @@ DEFAULTPAGE_IMAGE=nginx:latest
 DEFAULTPAGE_DOMAIN=static.{domain}
 """
 
-etherpad_env = """ETHERPAD_IMAGE=etherpad/etherpad:1.9
+ETHERPAD_ENV = """ETHERPAD_IMAGE=etherpad/etherpad:1.9
 ETHERPAD_DOMAIN=pad.{domain}
 ETHERPAD_DB_IMAGE=postgres:16-alpine
 """
 
-hedgedoc_env = """HEDGEDOC_IMAGE=quay.io/hedgedoc/hedgedoc:latest
+HEDGEDOC_ENV = """HEDGEDOC_IMAGE=quay.io/hedgedoc/hedgedoc:latest
 HEDGEDOC_DOMAIN=md.{domain}
 HEDGEDOC_DB_IMAGE=postgres:16-alpine
 """
@@ -96,13 +111,13 @@ app_name_map = {'infrastructure': 'Infrastructure Services', 'nextcloud': 'Nextc
                 'static': 'Static apps', 'etherpad': 'Etherpad',
                 'hedgedoc': 'Hedgedoc Markdown Editor'}
 
-app_var_map = {'infrastructure': infrastructure_env, 'nextcloud': nextcloud_env,
-               'kanboard': kanboard_env, 'tools': tools_env, 'moodle': moodle_env,
-               'static': static_env, 'etherpad': etherpad_env, 'hedgedoc': hedgedoc_env}
+app_var_map = {'infrastructure': INFRASTRUCTURE_ENV, 'nextcloud': NEXTCLOUD_ENV,
+               'kanboard': KANBOARD_ENV, 'tools': TOOLS_ENV, 'moodle': MOODLE_ENV,
+               'static': STATIC_ENV, 'etherpad': ETHERPAD_ENV, 'hedgedoc': HEDGEDOC_ENV}
 
 
 def create_logger():
-    global logger
+    """Create and configure a logger for logging to file and stdout."""
     logger.setLevel(logging.DEBUG)
     log_to_file = logging.handlers.RotatingFileHandler(
         'SchoolAppServer.log', maxBytes=262144, backupCount=5)
@@ -114,6 +129,7 @@ def create_logger():
 
 
 def parse_arguments():
+    """Parse command line arguments and return the given arguments."""
     parser = ArgumentParser(description='Administrative tool for SchoolAppServer.')
     parser.add_argument('-i', '--initial-setup', action='store_true',
                         help='set up all initial configuration and secret files')
@@ -124,8 +140,8 @@ def parse_arguments():
 
 
 def prepare_cli_interface():
+    """Set up style, key bindings and autocompletion for command line interface."""
     bindings = KeyBindings()
-
     @bindings.add('c-x')
     def _(event):
         event.app.exit()
@@ -154,7 +170,12 @@ def prepare_cli_interface():
 
 
 def create_password(length=25):
-    # create secure password of a given length (https://docs.python.org/3/library/secrets.html#recipes-and-best-practices)
+    """
+    Create a secure password of a given length. The password should contain at
+    least one lower case character, one upper case character and one digit.
+
+    Source: https://docs.python.org/3/library/secrets.html#recipes-and-best-practices
+    """
     alphabet = string.ascii_letters + string.digits
     while True:
         password = ''.join(secrets.choice(alphabet) for i in range(length))
@@ -166,6 +187,7 @@ def create_password(length=25):
 
 
 def find_all_secrets():
+    """Iterate over the Docker Compose files of each stack and find all secrets defined there."""
     all_secrets = []
     for path in Path('.').glob('*/docker-compose.yml'):
         data = yaml.safe_load(path.open())
@@ -176,50 +198,55 @@ def find_all_secrets():
 
 
 def create_secret_files():
+    """
+    Create all files with secrets referenced in the Docker Compose files and
+    fill them with long passwords.
+    """
     smtp_password = prompt('Please enter the SMTP password: ', is_password=True)
     for app, filename in find_all_secrets():
         filepath = Path(app) / filename
         if 'smtp_password' in filename:
             # handle SMTP password files
-            with open(filepath, 'w') as f:
+            with open(filepath, 'w', encoding='utf-8') as f:
                 f.write(smtp_password)
-                logger.debug(f'Writing SMTP password to file: {filename}')
+                logger.debug('Writing SMTP password to file: %s', filename)
         elif 'telegram' in filename:
             # handle telegram URL files
-            with open(filepath, 'w') as f:
+            with open(filepath, 'w', encoding='utf-8') as f:
                 bot_id = prompt('Please enter the Telegram bot id: ')
                 bot_id = '[bot_id]' if not bot_id else bot_id
                 chat_id = prompt('Please enter the Telegram chat id: ')
                 chat_id = '[chat_id]' if not chat_id else chat_id
                 url = f'telegram://{bot_id}@telegram/?channels={chat_id}'
                 f.write(url)
-                logger.debug(f'Writing Telegram IDs to file: {filename}')
+                logger.debug('Writing Telegram IDs to file: %s', filename)
         else:
             # handle all other files by just filling them with a long password
-            with open(filepath, 'w') as f:
+            with open(filepath, 'w', encoding='utf-8') as f:
                 f.write(create_password())
-                logger.debug(f'Writing password to secrets file: {filename}')
+                logger.debug('Writing password to secrets file: %s', filename)
 
 
 def replace_string_in_file(filename, old_string, new_string):
-    # replacing string in file (https://stackoverflow.com/a/20593644)
+    """Replacing string in file (https://stackoverflow.com/a/20593644)"""
     with fileinput.FileInput(Path(filename), inplace=True, backup='.bak') as file:
         for line in file:
             python_print(line.replace(old_string, new_string), end='')
 
 
 def replace_mail_address_in_files(mail_address):
-    # replace mail address in files
+    """Replace a given mail address in all configuration files."""
     placeholder = 'mail@example.com'
     for path in Path('.').glob('*/*.yml'):
         if path.is_file():
-            with open(path, 'r') as file:
+            with open(path, 'r', encoding='utf-8') as file:
                 if placeholder in file.read():
-                    log.debug(f'Found mail address to be replaced: {path}')
+                    logger.debug('Found mail address to be replaced: %s', path)
                     replace_string_in_file(path, placeholder, mail_address)
 
 
 def do_initial_setup():
+    """Initializing all configuration and secret files."""
     print(' *** Initializing all configuration and secret files *** ')
     # input all information from user
     mail_validator = Validator.from_callable(
@@ -249,13 +276,14 @@ def do_initial_setup():
         for p in parameter_names:
             parameters[p] = prompt(f'Please enter parameter "{p}": ')
         filename = Path(app, '.env')
-        logger.debug(f'Writing env vars to file: {filename}')
-        with open(filename, 'w') as f:
+        logger.debug('Writing env vars to file: %s', filename)
+        with open(filename, 'w', encoding='utf-8') as f:
             f.write(env_vars.format(**parameters))
     Path(INITIAL_SETUP_MARKER_FILE).touch()
 
 
 def output_status(docker_clients):
+    """Outputs status information about all stacks and their respective containers."""
     print(' *** Stacks and Container *** \n')
     for app, docker in docker_clients.items():
         container = docker.compose.ps(all=True)
@@ -266,26 +294,28 @@ def output_status(docker_clients):
 
 
 def start_app(docker_clients, app):
+    """Start all containers of a specific Docker stack."""
     print(f' *** Starting app {app} *** \n')
     docker_client = docker_clients[app]
     docker_client.compose.up(services=None, build=False, detach=True, pull='missing')
 
 
 def stop_app(docker_clients, app):
+    """Stop all containers of a specific Docker stack."""
     print(f' *** Stopping app {app} *** \n')
     docker_client = docker_clients[app]
     docker_client.compose.down()
 
 
 def check_if_initial_setup_completed():
-    # check whether initial setup has been executed
+    """Check whether initial setup has been executed."""
     if Path(INITIAL_SETUP_MARKER_FILE).is_file():
         return True
-    else:
-        return False
+    return False
 
 
 def main():
+    """Start command line interface and wait for commands."""
     # check for commandline arguments
     args = parse_arguments()
     if args.initial_setup:
@@ -296,7 +326,7 @@ def main():
     prompt_message = [('class:pound', '\n ➭ ')]
     # prepare instances of Docker client (Python-on-Whales: https://gabrieldemarmiesse.github.io/python-on-whales/sub-commands/compose/)
     docker_clients = {}
-    for app, env_vars in app_var_map.items():
+    for app, _ in app_var_map.items():
         docker = DockerClient(compose_files=[Path(app) / 'docker-compose.yml'], compose_env_file=Path(app) / '.env')
         docker_clients[app] = docker
     print(f'{APP} {VERSION}')
@@ -305,12 +335,11 @@ def main():
             user_input = session.prompt(prompt_message)
             if not user_input:
                 continue
-            else:
-                user_input = user_input.split()
+            user_input = user_input.split()
             command, args = user_input[0], user_input[1:]
         except KeyboardInterrupt:
             return
-        if command == 'exit' or command == 'quit':
+        if command in ('exit', 'quit'):
             return
         if command != 'setup' and not check_if_initial_setup_completed():
             print('Please start initial setup first by entering the command "setup"!')
@@ -319,33 +348,33 @@ def main():
             output_status(docker_clients)
         elif command == 'start':
             if args:
-                if args[0] in app_name_map.keys():
+                if args[0] in app_name_map:
                     start_app(docker_clients, args[0])
                 elif args[0] == 'all':
-                    for app in app_name_map.keys():
+                    for app in app_name_map:
                         start_app(docker_clients, app)
                 else:
                     print('Given app not available.')
             else:
                 results_array = checkboxlist_dialog(
                     title="Start apps", text="Which apps should be started?",
-                    values=[(k, v) for k, v in app_name_map.items()]
+                    values=list(app_name_map.items())
                 ).run()
                 for app in results_array:
                     start_app(docker_clients, app)
         elif command == 'stop':
             if args:
-                if args[0] in app_name_map.keys():
+                if args[0] in app_name_map:
                     stop_app(docker_clients, args[0])
                 elif args[0] == 'all':
-                    for app in app_name_map.keys():
+                    for app in app_name_map:
                         stop_app(docker_clients, app)
                 else:
                     print('Given app not available.')
             else:
                 results_array = checkboxlist_dialog(
                     title="Stop apps", text="Which apps should be stopped?",
-                    values=[(k, v) for k, v in app_name_map.items()]
+                    values=list(app_name_map.items())
                 ).run()
                 for app in results_array:
                     stop_app(docker_clients, app)
